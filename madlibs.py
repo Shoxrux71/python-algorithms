74a9d8da7053b98a2a8c0ccc51f1f1f77f09227f
Shohrux(ショフルフ） <63747328+Shoxrux71@users.noreply.github.com>
2025-05-01 03:41:56 +0900

    new

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adjective1 = input("Enter an adjective: ");
+noun1 = input("Enter a noun: ");
+adjective2 = input("Enter another adjective: ");
+verb1 = input("Enter another verb: ");
+adjective3 = input("Enter another adjective: ");
+
+print(f"Once upon a time, there was a {adjective1}");
+print(f"who lived in a {noun1}.");
+print(f"One day,{noun1} they {verb1} a {adjective2} ");
+print(f"and it was {adjective3}.");
+print("The end.");
--- typecasting.py
+++ typecasting.py
@@ -6,6 +6,15 @@ isStudent = True;
 new = bool(age);
 print(new - 0.4);
 print(type(new));
-
+#input()orqali malumot olsa bolar ekan va uni ozgaruvchiga olish kerak,shundan song foydalansa boladi"!
 name = input("isming nima?: ");
+
+
+age = input(f"yoshni kiriting: ");
+age = int(age);
+print(f"foydalanuvchi yoshi: {age}");
 print(f"foydalanuvchi ismi: {name}");
+if age > 18:
+    print("alkogol sotib olsa boladi ");
+else:
+    print("alkogol sotib olish mumkin emas");
