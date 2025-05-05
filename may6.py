d8f2ad0cb4ef82d64497211ac84ed7c320174f51
Shohrux(ショフルフ） <63747328+Shoxrux71@users.noreply.github.com>
2025-05-06 04:52:18 +0900

    today i am learned functions and math

--- may6.py
+++ may6.py
@@ -1,4 +1,25 @@
-friends = 5;
+#friends = 19;
+#friends = friends % 4;
+#print(friends);
 
-friends = friends * 7;
-print(friends);
+
+#x = 3.14
+#y = 4
+#z = 5
+#result = round(x);
+#result2 = abs(y);
+#print(result);
+#rint(result2);
+#result = pow(4,3);
+#print(result);
+#print(pow(3,4));
+
+
+import math
+x = 16
+result = math.sqrt(x);
+print(result);
+
+radius = float(input("enter the radius of the circle: "));
+circumference = 2 * math.pi * radius;
+print(f"The circumference of the circle is: {circumference}");
