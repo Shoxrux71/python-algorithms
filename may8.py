404813ccf80e86c08ae6f5b3dc0fa342657b8bfd
Shohrux(ショフルフ） <63747328+Shoxrux71@users.noreply.github.com>
2025-05-08 02:49:51 +0900

    today i learn new information

---
+++ may8.py
@@ -0,0 +1,41 @@
+import math;
+#radius = float(input("radius: "));
+#circumference = 2 * math.pi * radius;
+#print(f"circumference:  {round(circumference, 2)}");
+
+#radius = float(input("radius: "));
+#area = math.pi * pow(radius, 2);
+#print(f"The area of the circle is: {round(area, 2)}");
+
+
+#a = float(input("enter side a: "));
+#b = float(input("enter side b: "));
+#c = math.sqrt(pow(a,2) + pow(b,2));
+#print(f"side c: {c}");
+
+#if 
+#age = float(input("enter your age: "));
+
+
+#if age >= 18:
+   # print(f"you are signed up: {age}");
+#elif age < 0:
+  #  print("you are not born yet");
+#else:
+ #   print(f"you are not signed up: {age}");
+
+#response = input("would you like food? (yes/no): ");
+
+#if response == "yes":
+ #   print("your food have been ordered");
+
+#else:
+ #   print("your food have not been ordered");
+
+
+name = input("enter name: ");
+if name == "":
+    print("you have not entered a name");
+else:
+    print(f"your name is {name}");
+#    print("your name is not entered");
